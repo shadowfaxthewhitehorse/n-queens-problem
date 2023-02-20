8e1a37d15499e632dbff2c26701e34125465246f
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2023-02-20 15:01:06 +0530

    Update n-queens-problem-classic.py

--- n-queens-problem-classic.py
+++ n-queens-problem-classic.py
@@ -55,6 +55,23 @@ def print_board(board):
         print(" ".join("Q" if val == 1 else "." for val in row))
     print()
 
+# DEVNOTES
+# 
+#In this program, we first create a two-dimensional list board of size n x n and initialize all elements to 0. We then create an empty list solutions to 
+# store the solutions.
+#
+# We then call the solve_n_queens_helper function to solve the n-queens problem. This function takes the current state of the board, the current column, and 
+# the list of solutions as input. It tries to place a queen in each row of the current column and recursively calls itself with the updated board and column 
+# number. If a solution is found, it appends a copy of the board to the solutions list.
+#
+# The is_safe function checks whether it is safe to place a queen in a particular cell. It checks the row, the upper diagonal, and the lower diagonal to 
+# ensure that no other queen can capture the queen being placed.
+#
+# The print_board function simply prints the current state of the board in a readable format.
+#
+# Finally, we test the program with a 4x4 board by calling solve_n_queens and printing the solutions using print_board. You can change the value of n to 
+# solve the n-queens problem for a different board size.
+
 # Test the program with a 4x4 board
 solutions = solve_n_queens(4)
 for solution in solutions:
