c0926a3a0e3e9f3e1a8cfefb50dd376ea15a3dbf
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2023-02-20 14:55:22 +0530

    Create n-queens-problem.go

---
+++ n-queens-problem.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type position struct {
+	row, col int
+}
+//
+func solveNQueens(n int) [][]position {
+	board := make([][]bool, n)
+	for i := range board {
+		board[i] = make([]bool, n)
+	}
+	solutions := [][]position{}
+	solveNQueensHelper(board, 0, &solutions)
+	return solutions
+}
+
+func solveNQueensHelper(board [][]bool, col int, solutions *[][]position) {
+	// Base case: all queens have been placed
+	if col == len(board) {
+		positions := []position{}
+		for i := 0; i < len(board); i++ {
+			for j := 0; j < len(board); j++ {
+				if board[i][j] {
+					positions = append(positions, position{i, j})
+				}
+			}
+		}
+		*solutions = append(*solutions, positions)
+		return
+	}
+
+	// Try placing a queen in each row of the current column
+	for row := 0; row < len(board); row++ {
+		if isSafe(board, row, col) {
+			board[row][col] = true
+			solveNQueensHelper(board, col+1, solutions)
+			board[row][col] = false
+		}
+	}
+}
+
+func isSafe(board [][]bool, row int, col int) bool {
+	// Check the row
+	for i := 0; i < col; i++ {
+		if board[row][i] {
+			return false
+		}
+	}
+
+	// Check the upper diagonal
+	i, j := row, col
+	for i >= 0 && j >= 0 {
+		if board[i][j] {
+			return false
+		}
+		i--
+		j--
+	}
+
+	// Check the lower diagonal
+	i, j = row, col
+	for i < len(board) && j >= 0 {
+		if board[i][j] {
+			return false
+		}
+		i++
+		j--
+	}
+
+	// Check for knight attacks
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board); j++ {
+			if board[i][j] && (math.Abs(float64(row-i)) == 2 && math.Abs(float64(col-j)) == 1 ||
+				math.Abs(float64(row-i)) == 1 && math.Abs(float64(col-j)) == 2) {
+				return false
+			}
+		}
+	}
+
+	// All checks passed, it is safe to place a queen here
+	return true
+}
+
+func printBoard(board [][]bool) {
+	for i := range board {
+		for j := range board[i] {
+			if board[i][j] {
+				fmt.Print("Q ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func main() {
+	// Test the program with a 4x4 board
+	solutions := solveNQueens(4)
+	for _, solution := range solutions {
+		board := make([][]bool, 4)
+		for i := range board {
+			board[i] = make([]bool, 4)
+		}
+		for _, pos := range solution {
+			board[pos.row][pos.col] = true
+		}
+		printBoard(board)
+	}
+}
